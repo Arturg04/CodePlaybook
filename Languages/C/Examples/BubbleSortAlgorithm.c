b3af533867c6b81174b29d850a2f4bae9041e0f8
Artur Gonçalves <artur.13.goncalves@gmail.com>
2024-11-18 17:58:05 +0000

    Add Debug Coments

--- Languages/C/Examples/BubbleSortAlgorithm.c
+++ Languages/C/Examples/BubbleSortAlgorithm.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
 
+
+// Uncomment the line below to enable debug mode
+// #define DEBUG
+
+// Bubble sort algorithm
+// Sorting in ascending order
+// The algorithm compares each pair of adjacent elements and swaps them if they are in the wrong order
+// The pass through the list is repeated until no swaps are needed
 void static BubbleSort(int arr[], int n) {
 	int i = 0, j = 0, temp = 0;
 
 	while (i < n) {
 		j = 0;
 		while (j < n - i - 1) {
-			if (arr[j] > arr[j + 1]) {
-				// Swap
+			if (arr[j] > arr[j + 1]) 
+			{
+				if (DEBUG) printf("Swap %d and %d\n", arr[j], arr[j + 1]);
 				temp = arr[j];
 				arr[j] = arr[j + 1];
 				arr[j + 1] = temp;
 			}
 			j++;
+
+		}
+		if (DEBUG) {
+			printf("Pass %d: ", i);
+			for (int k = 0; k < n; k++) printf("%d ", arr[k]);
+			printf("\n");
 		}
 		i++;
 	}
